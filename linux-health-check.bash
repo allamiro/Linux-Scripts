cfe57866a58afed15ff7ec963bda657515551607
Tamir Suliman <allamiro@gmail.com>
2020-11-17 19:50:47 +0300

    Update linux-health-check.bash

--- linux-health-check.bash
+++ linux-health-check.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Description: Script use to check the health of the Linux systems
 CHECK_FLAG=0
-Report=/tmp/health-check-report.txt
+Report=/tmp/health-check-report_$date.txt
 # If report file and approved user file exists
 if [[ -f "$Report" && -f "$Approved" ]]; then
 
@@ -10,4 +10,13 @@ echo "Health Check Report exist"
 echo "Approved User file"
 
 fi
+
+### Check for unapproved user accounts -- add your approved user accounts here
+APPROVED_USERS="root user1 user2"
+for APPROVED_USER in $APPROVED_USERS
+do
+        echo $APPROVED_USER >> /tmp/approved_users_$date
+done
+
+### Check for unapproved interactive accounts
 
