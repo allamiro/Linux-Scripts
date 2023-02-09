b7db905071a4f7a7fe87456bf2ec00eac6a5f35d
Tamir Suliman <allamiro@gmail.com>
2023-02-09 17:58:41 +0100

    Update logstash-to-cef.py

--- logstash-to-cef.py
+++ logstash-to-cef.py
@@ -1,9 +1,44 @@
 # Author Tamir Suliman
 # data 02 - 9 -2022
-
-# Reading data from logstasy output over TCP 
-
+# Import libraries 
 import json 
+import socket 
+import os 
+
+# Reading data from logstash output over TCP 
+
+def receive_logs_over_syslog(port):
+    # Create a TCP/IP socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Bind the socket to a specific address and port
+    server_address = ('localhost', port)
+    print(f"starting up on {server_address}")
+    server_socket.bind(server_address)
+    # Listen for incoming connections
+    server_socket.listen(1)
+    while True:
+        # Wait for a connection
+        print("waiting for a connection")
+        client_socket, client_address = server_socket.accept()
+        print(f"connection from {client_address}")
+        # Receive the incoming data
+        data = client_socket.recv(4096)
+        # If there's no data, the connection is likely closed
+        if not data:
+            print("no data from client")
+            client_socket.close()
+            break
+        # Print the incoming data
+        print(f"received data: {data.decode()}")
+
+        # Send a response back to the client
+        client_socket.sendall(b"ACK")
+
+    # Clean up the server socket
+    server_socket.close()
+
+
+
 def json_to_cef(message):
    The CEF header and extension fields
     header = 'CEF:0|json-to-cef|json-to-cef|1.0|'
@@ -26,3 +61,5 @@ if __name__ == '__main__':
     print(cef_format)
 
 
+if __name__ == '__main__':
+   
