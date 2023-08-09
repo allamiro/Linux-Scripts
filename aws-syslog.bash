6935fc199779fed8453350a78f1781f812d53b49
Tamir Suliman <allamiro@gmail.com>
2023-08-09 19:44:44 +0200

    Update aws-syslog.bash

--- aws-syslog.bash
+++ aws-syslog.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Function to detect new disk
 detect_new_disk() {
   for disk in /sys/block/sd*; do
     if ! grep -q "${disk##*/}" /etc/mtab; then
@@ -8,14 +9,16 @@ detect_new_disk() {
     fi
   done
 }
+
+# Function to create LVM and format with XFS
 create_lvm() {
   pvcreate "$DISK" || { echo "Failed to create physical volume."; exit 1; }
   vgcreate myvg "$DISK" || { echo "Failed to create volume group."; exit 1; }
   lvcreate -l 100%FREE -n mylv myvg || { echo "Failed to create logical volume."; exit 1; }
-  mkfs.ext4 /dev/myvg/mylv || { echo "Failed to format logical volume."; exit 1; }
+  mkfs.xfs /dev/myvg/mylv || { echo "Failed to format logical volume with XFS."; exit 1; }
   mkdir -p /mnt/mylv
   mount /dev/myvg/mylv /mnt/mylv || { echo "Failed to mount logical volume."; exit 1; }
-  echo '/dev/myvg/mylv /mnt/mylv ext4 defaults 0 0' >> /etc/fstab
+  echo '/dev/myvg/mylv /mnt/mylv xfs defaults 0 0' >> /etc/fstab
 }
 
 detect_new_disk
@@ -25,11 +28,11 @@ if [ -z "$DISK" ]; then
 fi
 
 echo "New disk detected: $DISK"
-read -p "Do you want to format this disk as LVM? [y/N]: " response
+read -p "Do you want to format this disk with XFS and mount it? [y/N]: " response
 
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
   create_lvm
-  echo "Disk $DISK has been formatted as LVM and mounted at /mnt/mylv"
+  echo "Disk $DISK has been formatted with XFS and mounted at /mnt/mylv"
 else
   echo "Aborted."
 fi
